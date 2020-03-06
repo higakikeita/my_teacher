8827e50039b1a840b208bea1e2966a6c979f86a5
@higakikeita <higaki.keichan@gmail.com>
2020-03-07 02:24:46 +0900

    詳細表示サーバサイド設定

--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -1,6 +1,10 @@
 class TeachersController < ApplicationController
+before_action :set_teacher,only: [:index,:show]
+
   def index
+      
   end
+
   def new
     @teacher = Teacher.new
     
@@ -11,8 +15,14 @@ class TeachersController < ApplicationController
     @teacher.save
     redirect_to root_path
   end
+  def show
+  end
+  
 private
   def teacher_params
     params.require(:teacher).permit(:name,:category_id,:price,:explain,:sex,:brand_id,:status,:postage,:shipping_date,:prefecture,:university)
+  end
+  def set_teacher
+    @teacher = Teacher.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
-  get 'top/index'
-
-  get 'teachers/index'
-
+  
   root "top#index"
-  resources :teachers, only: [:index,:new,:create]
+  
+  resources :teachers, only: [:index,:new,:create,:show]
 end
