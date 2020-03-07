33e93afb91fd61f823a856e01ae6b00fe9fd78bf
@higakikeita <higaki.keichan@gmail.com>
2020-03-07 17:44:03 +0900

    画像投稿処理コントローラ

--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -7,20 +7,24 @@ before_action :set_teacher,only: [:index,:show]
 
   def new
     @teacher = Teacher.new
-    
+    @teacher.images.new
   end
   def create
     
     @teacher = Teacher.new(teacher_params)
-    @teacher.save
-    redirect_to root_path
+    if @product.images.present?
+      @teacher.save
+      redirect_to root_path
+    else
+      redirect_to root_path
+    end
   end
   def show
   end
   
 private
   def teacher_params
-    params.require(:teacher).permit(:name,:category_id,:price,:explain,:sex,:brand_id,:status,:postage,:shipping_date,:prefecture,:university)
+    params.require(:teacher).permit(:name,:category_id,:price,:explain,:sex,:brand_id,:status,:postage,:shipping_date,:prefecture,:university,images_attributes: [:teacher_image,:_destroy,:id])
   end
   def set_teacher
     @teacher = Teacher.find(params[:id])
