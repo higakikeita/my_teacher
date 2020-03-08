baf3d121cec94e8b65e14e5fe52013bbeb2f7c8a
@higakikeita <higaki.keichan@gmail.com>
2020-03-08 19:20:57 +0900

    ルーティングcollection追加

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,9 @@ Rails.application.routes.draw do
   root "top#index"
   
   resources :teachers, only: [:index,:new,:create,:show]
+    collection do
+      get 'search_child', defaults: { format: 'json' }
+      get 'search_grandchild', defaults: { format: 'json' }
+    end
   resources :categories, only: [:index, :show]
 end
