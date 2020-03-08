3410ca2a97539fb4e5052b95c3efa3fdb6e5186b
@higakikeita <higaki.keichan@gmail.com>
2020-03-08 19:18:15 +0900

    カテゴリーAjax通信化

--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -25,6 +25,14 @@ class TeachersController < ApplicationController
   def show
     @teacher = Teacher.find(params[:id])
   end
+  def search_child
+    respond_to do |format|
+      format.html
+      format.json do
+        @children = Category.find(params[:parent_id]).children
+      end
+    end
+  end
   
 private
   def teacher_params
