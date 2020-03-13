e48b0b492a02ca016e69f4d1270156f4d9cfad5e
@higakikeita <higaki.keichan@gmail.com>
2020-03-13 22:15:25 +0900

    swipeの導入

---
+++ app/assets/javascripts/swipe.js
@@ -0,0 +1,21 @@
+window.addEventListener('DOMContentLoaded', function () {
+  var mySwiper = new Swiper('.swiper-container', {
+    autoHeight: true,
+    loop: true,
+    keyboard: true,
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false,
+      reverseDirection: false
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev'
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true
+    }
+  });
+}, false);
