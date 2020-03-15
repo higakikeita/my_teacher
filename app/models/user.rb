ca8afb3f41e5e4cad990b0759bcf6138c93435fb
@higakikeita <higaki.keichan@gmail.com>
2020-03-15 15:26:55 +0900

    マイページ必要項目記入

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :teachers
+  mount_uploader :img_name, ImageUploader
 end
