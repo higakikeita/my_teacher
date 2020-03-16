93381c3bacdb163c257a53c01330dba5e3efe212
@higakikeita <higaki.keichan@gmail.com>
2020-03-16 15:47:10 +0900

    ヘッダービュー修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,8 @@ class UsersController < ApplicationController
   def show
     @user = User.find_by(id: params[:id])
   end
+  # def likes
+  #   @user = User.find_by(id: params[:id])
+  #   @likes = Like.where(user_id:@user.id)
+  # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,5 @@ Rails.application.routes.draw do
   end
   resources :categories, only: [:index, :show]
   resources :users, only: :show
+  get "users/:id/likes" => "users#likes"
 end
