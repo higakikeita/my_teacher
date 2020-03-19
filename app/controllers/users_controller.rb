db0d7fc1500a8fbb91150722c3bd8fb47571e9b6
@higakikeita <higaki.keichan@gmail.com>
2020-03-20 01:03:14 +0900

    ユーザー側からclip呼び出し

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,19 @@
 class UsersController < ApplicationController
+  before_action :set_clips 
   def show
     @user = User.find_by(id: params[:id])
+    @clip= Clip.new
+    @clips = @user.clips.includes(:teacher).all
   end
   # def likes
   #   @user = User.find_by(id: params[:id])
   #   @likes = Like.where(user_id:@user.id)
   # end
+  private
+  def set_clips
+    @teacher = Teacher.find(params[:id])
+    @clips = Clip.where(teacher_id: @teacher.id).all
+    
+  end
 end
+
