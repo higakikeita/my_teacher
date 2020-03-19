ab9287e5a3a485f0d2e09a27fc10ea80516b74bc
@higakikeita <higaki.keichan@gmail.com>
2020-03-19 11:36:37 +0900

    migrationファイルの編集

--- db/migrate/20200319022430_create_clips.rb
+++ db/migrate/20200319022430_create_clips.rb
@@ -1,8 +1,11 @@
 class CreateClips < ActiveRecord::Migration[5.0]
   def change
     create_table :clips do |t|
-
+      t.references :user, null:false
+      t.references :teacher, null:false
       t.timestamps
     end
+    # add_index :clips, :user_id
+    # add_index :clips, :teacher_id
   end
 end
