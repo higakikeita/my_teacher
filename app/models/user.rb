fe2a0c1b28ce5e6bd3e3bef67781124e80f9f980
@higakikeita <higaki.keichan@gmail.com>
2020-03-19 11:32:21 +0900

    アソシエーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :teachers
   has_many :likes
+  has_many :clips, dependent: :destroy
+  has_many :teachers, through: :clips
   mount_uploader :img_name, ImageUploader
   def already_liked?(teacher)
     self.likes.exists?(teacher_id: teacher.id)
