70abb74fae1345a8e4aa9454a8f9c2870581f090
@higakikeita <higaki.keichan@gmail.com>
2020-03-25 01:08:59 +0900

    コントローラ編集

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,20 @@
 class CommentsController < ApplicationController
+  before_action :set_teacher
+  def new
+    @comment = Comment.new
+  end
+  def create
+    @comment = @teacher.comments.new(comment_params)
+    @teacher =@comment.teacher
+    @comment.save
+      
+    
+  end
+  private
+    def comment_params
+      params.require(:comment).permit(:comment,:teacher_id).merge(user_id: current_user.id)
+    end
+  def set_teacher
+    @teacher = Teacher.find(params[:id])
+  end 
 end
