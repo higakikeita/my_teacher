3a9f87a5d694bda4dc6b61dcaf7dc2f8556b535e
@higakikeita <higaki.keichan@gmail.com>
2020-03-25 23:52:23 +0900

    コントローラ編集

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,15 @@
 class MessagesController < ApplicationController
+  def new
+    @message = Comment.new
+  end
+  def create
+    @message=@user.messages.new(message_params)
+    @user=@message.user
+    @message.save
+  end
+  private
+  def message_params
+    params.require(:message).permit(:message,:user_id).merge(:teacher_id)
+  end
+
 end
