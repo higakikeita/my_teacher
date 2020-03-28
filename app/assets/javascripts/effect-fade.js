2386a681a0a4204c319151b7b91805ef282bd841
@higakikeita <higaki.keichan@gmail.com>
2020-03-28 20:21:19 +0900

    effect-fade実装

---
+++ app/assets/javascripts/effect-fade.js
@@ -0,0 +1,12 @@
+$(function () {
+  $(window).scroll(function () {
+    $('.effect-fade').each(function () {
+      var elemPos = $(this).offset().top;
+      var scroll = $(window).scrollTop();
+      var windowHeight = $(window).height();
+      if (scroll > elemPos - windowHeight) {
+        $(this).addClass('effect-scroll');
+      }
+    });
+  });
+});
