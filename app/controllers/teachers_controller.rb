392b0daf288cc3451ecf5912b5c909aaea21b4bc
@higakikeita <higaki.keichan@gmail.com>
2020-04-03 01:17:38 +0900

    検索条件の修正

--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -77,6 +77,7 @@ before_action :set_ransack
   def search
     @q = Teacher.search(search_params)
     @teachers = @q.result(distinct: true)
+    
   end 
   
 private
@@ -93,6 +94,6 @@ private
     @q        = Teacher.ransack(params[:q])
   end
   def search_params
-    params.permit(:q)
+    params.require(:q).permit(:sorts,:search_order,:name_cont,:brand_cont,:size_id_eq,:status_eq,:postage_eq,:purchase_status_id_eq,:category_grandparent_id_eq,:category_parent_id_eq,:category_id_eq,:price_lteq,:price_gteq,:explain_cont,:prefecture_eq)
   end
 end
